0d67e86aab31ca9594299aaf5c5c12bc8a25ee3f
Abigail Lin <alin50@stuy.edu>
2025-01-05 20:47:18 -0500

    was able to fix persistant_server by checking for sigpipe

--- persistant_server.c
+++ persistant_server.c
@@ -1,17 +1,49 @@
 #include "pipe_networking.h"
+int from_client = 0;
+int to_client = 0;
+
+void sighandler(int signo){
+  if (signo == SIGPIPE){
+    printf("Client disconnected\n");
+  }
+  else if (signo == SIGINT){
+    printf("\nServer closing...\n");
+    close(from_client);
+    close(to_client);
+    remove(WKP);
+    printf("Removed WKP\n");
+    exit(0);
+  }
+}
 
 int main() {
-  int to_client;
-  int from_client;
-  char buffer[BUFFER_SIZE];
+  srand(getpid());
+
+  // SIGINT
+  signal(SIGINT,sighandler);
+  signal(SIGPIPE,sighandler);
 
   while (1){
+
+    // multiple clients
+    printf("Waiting for a new client...\n");
+
     from_client = server_handshake( &to_client );
-    while (read (from_client, buffer, sizeof(buffer))){
-      printf("Recieved: %d", buffer);
-      write(to_client, buffer, strlen(buffer) + 1);
+    
+    while (1){
+      int rd = rand() % 100;
+      printf("Message Sent to Client: %d\n",rd);
+      if ( write(to_client, &rd, sizeof(rd)) == -1){
+        perror("client disconnected");
+        break;
+      }
+      sleep(1);
     }
+    printf("Client disconnect\n");
     close(to_client);
     close(from_client);
+    to_client = 0;
+    from_client = 0;
   }
+  return 0;
 }
